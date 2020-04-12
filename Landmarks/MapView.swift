164f2db61fed6befcf77b2aad5a7975aa9bbd36e
Landon Woerdeman <13247759+lwoerdeman@users.noreply.github.com>
2020-04-11 19:57:04 -0500

    add map view

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,33 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Landon Woerdeman on 4/11/20.
+//  Copyright © 2020 Landon Woerdeman. All rights reserved.
+//
+
+import SwiftUI
+
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    typealias UIViewType = MKMapView
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
